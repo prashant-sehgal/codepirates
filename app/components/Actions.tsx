c4da09ee400aedab6e78789136075883144699f8
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2025-07-26 19:02:09 +0530

    feat: replaced icon button with multi-functional secondary button.

--- app/components/Actions.tsx
+++ app/components/Actions.tsx
@@ -1,15 +1,21 @@
 'use client';
 import React from 'react';
-import IconButton from './IconButton';
 import { usePrompt } from '../contexts/PromptContext';
+import SecondaryButton from './SecondaryButton';
 
 export default function Actions() {
   const { query, submitQuery } = usePrompt();
 
   if (query.length > 0)
     return (
-      <IconButton icon="ri-send-plane-2-fill" onPress={() => submitQuery()} />
+      <SecondaryButton className="w-11" onPress={submitQuery}>
+        <i className="ri-send-plane-2-fill"></i>
+      </SecondaryButton>
     );
 
-  return <IconButton icon="ri-mic-line" onPress={() => ''} />;
+  return (
+    <SecondaryButton className="w-11" onPress={() => console.log('mic')}>
+      <i className="ri-mic-line"></i>
+    </SecondaryButton>
+  );
 }
---
+++ app/components/SecondaryButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function SecondaryButton(
+  props: Readonly<{
+    children: React.ReactNode;
+    className?: string;
+    onPress: () => void;
+  }>
+) {
+  return (
+    <button
+      className={`${props.className} rounded-md cursor-pointer bg-stone-900 hover:bg-stone-800`}
+      onClick={() => props.onPress()}
+    >
+      {props.children}
+    </button>
+  );
+}
